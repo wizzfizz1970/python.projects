218501905b89d18dec0cf5147b7e7ad04b5faa7c
Kevin Brown <s3831353@student.rmit.edu.au>
2024-11-21 22:16:04 +1100

    update chapter 5

---
+++ python Crash Course 3/Chapter 5/hello_admin.py
@@ -0,0 +1,8 @@
+usernames = ['adim','kevin','rebecca','joann','wayne']
+
+for username in usernames:
+    if username == 'adim':
+        print(f'Hello {username} would like to see status report')
+
+    else:
+        print(f'\nHello {username}, thankyou for logging in again')
